79404a51d2ce090d3e8133f0bf2413998da5927d
Mohamed Hassan Mohamed Althaf <hassan@surge.global>
2024-01-16 15:07:11 +0530

    feat: Add Categories Seeder

---
+++ database/seeders/CategoriesSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
+
+class CategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $validCategories = new Collection(['Small', 'Mid-sized', 'SUV', "Motorcycles"]);
+
+
+        $categories = $validCategories->map(function (string $category, $index) {
+            return [
+                'id' => $index + 1,
+                'name' => $category,
+            ];
+        });
+
+        Category::upsert($categories->toArray(), ['id'], ['name']);
+    }
+}
