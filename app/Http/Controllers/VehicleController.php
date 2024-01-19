22abb6b8f116b1bb5fd135853d5852173c33ac3b
Mohamed Hassan Mohamed Althaf <hassan@surge.global>
2024-01-19 15:46:39 +0530

    refactor: remove un-used import

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Vehicle;
 use Illuminate\Http\Request;
-use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class VehicleController extends Controller
